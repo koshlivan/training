8101c7813a371b0c0f6f28afa95c30526db9cf05
Ivan Koshliak <koshlivan@gmail.com>
2021-12-01 22:10:27 +0200

    start

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>title</title>
+</head>
+<body>
+    <div class="contain">
+        <nav class="menu">
+            <a href="#">Main Page</a>
+            <a href="#">Edit</a>
+            <a href="#">Login</a>
+            <a href="#">Register</a>
+            <a href="#">Logout</a>
+        </nav>
+        <div class="wraper">
+
+            <div class="side-info">
+                <img src="Photo.png" alt="Profile">
+                <h3 class="lblCont">Contact</h3>
+                <p class="p-info">+38(066)32-43-771<br>
+                koshlivan@gmail.com<br>
+                Ukraine, Zaporizhzhya
+                </p>
+                <h3 class="lblCont">Education</h3>
+                <p class="p-info">Software developer<br>
+                "IT Step" Academy<br>
+                2019-2022
+                </p>
+                <h3 class="lblCont">Skills</h3>
+                <ul class="ul-info">
+                    <li>php</li>
+                    <li>html</li>
+                    <li>css</li>
+                </ul>
+            </div>
+
+            <div class="main-info">
+                <div class="head">
+                    <h1>ivan koshliak</h1>
+                    <h2>web developer</h2>
+                </div>
+                <div class="remain-info">  
+                    <h3>Profile</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat tempora modi animi minima officiis sunt similique itaque quidem ab ut omnis porro nobis reiciendis eum maxime dolore sequi, sed vero.</p>
+                    <h3>Professional experiance</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta placeat quis iure modi nobis, repellat illum blanditiis, laboriosam, debitis ipsam eveniet saepe. Exercitationem, libero repellendus perferendis eveniet assumenda odit distinctio.</p>
+                </div> 
+            </div>
+
+        </div>
+    </div>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
